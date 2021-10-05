569c88de055fad47f20c3cae1727e4ba30517f25
felipeperet <sasdelli.pi@gmail.com>
2021-10-05 05:15:20 -0300

    modified main file

--- main.ml
+++ main.ml
@@ -8,7 +8,7 @@ type data_card = string array
 type data_base = { card_index : string -> int ;
                    data : data_card list }
 
-(* access to the field n of a card c *)
+(* access to the field of a card *)
 let field base name =
   let i = base.card_index name in fun (card : data_card) -> card.(i)
 
