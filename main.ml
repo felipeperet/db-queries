b8048a84c4fb966c8a749b325e0b29f89ed7c9e0
felipeperet <sasdelli.pi@gmail.com>
2021-10-12 01:31:14 -0300

    composing criteria

--- main.ml
+++ main.ml
@@ -65,17 +65,17 @@ let eq_ffield = tst_ffield (Float.equal)
 let lt_ffield = tst_ffield (Float.(<=))
 let gt_ffield = tst_ffield (Float.(>=))
 
-(*   We decide to represent dates in a card as a string with format dd.mm.yyyy.
- *   In order to be able to define additional comparisons, we also allow the replacement of the day,
+(*  We decide to represent dates in a card as a string with format dd.mm.yyyy.
+ *  In order to be able to define additional comparisons, we also allow the replacement of the day,
  *  month or year part with the underscore character ('_').
  *
- *   Dates are compared according to the lexicographic order of lists of integers of the form [year; month; day].
- *   To express queries such as: ``is before July 1998'', we use the date pattern: "_.07.1998".
+ *  Dates are compared according to the lexicographic order of lists of integers of the form [year; month; day].
+ *  To express queries such as: ``is before July 1998'', we use the date pattern: "_.07.1998".
  *
- *   Comparing a date with a pattern is accomplished with
+ *  Comparing a date with a pattern is accomplished with
  *  the function tst_dfield which analyses the pattern to create the ad hoc comparison function.
  *
- *   To define this generic test function on dates, we need a few auxiliary functions. *)
+ *  To define this generic test function on dates, we need a few auxiliary functions. *)
 
 let split_date = String.split ~on:'.'
 
@@ -106,8 +106,8 @@ let rec app_dtst r d1 d2 =
 
 (* We finally define the generic function tst_dfield
  * which takes as arguments a relation r, a database db, a pattern dp, a field name nm, and a card dc.
+ *
  * This function checks that the pattern and the field from the card satisfy the relation. *)
-
 let tst_dfield r db dp nm dc =
   r (ints_of_dpat dp) (ints_of_string (field db nm dc))
 
@@ -115,3 +115,26 @@ let tst_dfield r db dp nm dc =
 let eq_dfield = tst_dfield (app_dtst (=))
 let le_dfield = tst_dfield (app_dtst (<=))
 let ge_dfield = tst_dfield (app_dtst (>=))
+
+(* the test "is before July 1998" is written *)
+let before_july = ge_dfield base_ex "_.07.1998" "Date"
+
+(* Thus, we can consider a test as a function of type data_card -> bool.
+ *  We want to obtain boolean combinations of the results of such functions applied to a given card.
+ *  To this end, we implement the iterator:  *)
+let fold_funs b c fs dc =
+  List.fold_right fs ~f:(fun f -> c (f dc)) ~init:b
+(* where b is the base value, the function c is the boolean operator,
+ * fs is the list of test functions on a field, and dc is a card *)
+
+(* We can obtain the conjunction and the disjunction of a list of tests with: *)
+let and_fold fs = fold_funs true (&&) fs
+let or_fold fs = fold_funs false (||) fs
+
+(* negation of a test *)
+let not_fun f dc = not (f dc)
+
+(* using these combinators to define a selecion function for cards
+ * whose data field is included in a given range *)
+let date_interval db d1 d2 =
+  and_fold [(le_dfield db d1 "Date") ; (ge_dfield db d2 "Date")]
