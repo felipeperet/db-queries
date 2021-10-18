574ae6769cc4ed1c4c9dcacf13aea42257cfc619
felipeperet <sasdelli.pi@gmail.com>
2021-10-18 05:47:11 -0300

    refactored some code

--- bin/main.ml
+++ bin/main.ml
@@ -6,19 +6,27 @@ open Lib.Datatypes
 
 (* list of mail addresses of a given database *)
 let mail_addresses db =
-  let dcs = List.filter ~f:(Process.eq_sfield db "mail" "Pref") db.data in
+  let dcs
+    = List.filter
+      ~f:(Process.eq_sfield db "mail" "Pref") db.data in
   List.map ~f:(Format.line db ["Address"]) dcs
 
 (* list of email addresses of a given database *)
 let email_addresses db =
-  let dcs = List.filter ~f:(Process.eq_sfield db "email" "Pref") db.data in
+  let dcs =
+    List.filter
+      ~f:(Process.eq_sfield db "email" "Pref") db.data in
   List.map ~f:(Format.line db ["Email"]) dcs
 
-(* computing the state of the received fees during an interval *)
+(* computing the state of the received fees during
+ *  an interval *)
 let fees_state db d1 d2 =
-  let dcs = List.filter ~f:(Process.date_interval db d1 d2) db.data in
-  let ls  = List.map ~f:(Format.line db ["Date";"Amount"]) dcs in
-  let t   = Compute.total db dcs in (ls , t)
+  let dcs =
+    List.filter
+      ~f:(Process.date_interval db d1 d2) db.data in
+  let ls =
+    List.map ~f:(Format.line db ["Date";"Amount"]) dcs in
+  let t = Compute.total db dcs in (ls , t)
 
 (* main program *)
 let () =
@@ -35,8 +43,10 @@ let () =
     | 1 -> (List.iter ~f:print_string (mail_addresses db))
     | 2 -> (List.iter ~f:print_string (email_addresses db))
     | 3
-      -> (print_string "Start date: "; let d1 = Caml.read_line () in
-         print_string "End date: "  ; let d2 = Caml.read_line () in
+      -> (print_string "Start date: ";
+          let d1 = Caml.read_line () in
+          print_string "End date: "  ;
+          let d2 = Caml.read_line () in
          let ls, t = fees_state db d1 d2 in
          List.iter ~f:print_string ls;
          printf "Total: %f\n" t )
--- lib/process.ml
+++ lib/process.ml
@@ -6,19 +6,22 @@ open Datatypes
 
 (* STRING FIELDS *)
 
-let eq_sfield db s n dc = (String.equal s (field db n dc))
-let nonempty_sfield db n dc = not (String.equal "" (field db n dc))
+let eq_sfield db s n dc =
+  (String.equal s (field db n dc))
 
-(*-----------------------------------------------------------------------------------*)
+let nonempty_sfield db n dc =
+  not (String.equal "" (field db n dc))
 
+(*--------------------------------------------------*)
 (* FLOAT FIELDS *)
 
-let tst_ffield r v db n dc = r v (Float.of_string (field db n dc))
+let tst_ffield r v db n dc =
+  r v (Float.of_string (field db n dc))
 
 let eq_ffield = tst_ffield (Float.equal)
 let lt_ffield = tst_ffield (Float.(<=))
 
-(*-----------------------------------------------------------------------------------*)
+(*--------------------------------------------------*)
 
 (* DATE FIELDS *)
 
@@ -28,14 +31,18 @@ let split_date = String.split ~on:'.'
 
 let ints_of_string d =
   try match split_date d with
-      [d;m;y] -> [Int.of_string y; Int.of_string m; Int.of_string d]
+      [d;m;y] ->
+      [Int.of_string y; Int.of_string m; Int.of_string d]
     |  _      -> failwith "Bad date format"
   with Failure _ -> failwith "Bad date format"
 
 let ints_of_dpat d =
-  let int_of_stringpat = function "_" -> 0 | s -> Int.of_string s in
+  let int_of_stringpat =
+    function "_" -> 0 | s -> Int.of_string s in
   try match split_date d with
-      [d;m;y] -> [ int_of_stringpat y ; int_of_stringpat m ; int_of_stringpat d ]
+      [d;m;y] ->
+      [ int_of_stringpat y ; int_of_stringpat m ;
+        int_of_stringpat d ]
     | _ -> failwith "Bad date format"
   with Failure _ -> failwith "Bad date pattern"
 
@@ -67,24 +74,30 @@ let eq_dfield     = tst_dfield eq_dtst
 let after_dfield  = tst_dfield (cmp_dtst (<))
 let before_dfield = tst_dfield (cmp_dtst (>))
 
-(* Thus, we can consider a test as a function of type data_card -> bool.
- *  We want to obtain boolean combinations of the results of such functions applied to a given card.
+(* Thus, we can consider a test as a function
+ *  of type data_card -> bool.
+ *
+ *  We want to obtain boolean combinations of
+ *  the results of such functions applied to a given card.
  *  To this end, we implement the iterator:  *)
 
 let fold_funs b c fs dc =
   List.fold_right fs ~f:(fun f -> c (f dc)) ~init:b
 
-(* where b is the base value, the function c is the boolean operator,
- * fs is the list of test functions on a field, and dc is a card *)
+(* where b is the base value, the function c is
+ * the boolean operator,
+ * fs is the list of test functions
+ * on a field, and dc is a card *)
 
-(* We can obtain the conjunction and the disjunction of a list of tests with: *)
+(* We can obtain the conjunction and the disjunction
+ *  of a list of tests with: *)
 let and_fold fs = fold_funs true (&&) fs
 let or_fold  fs = fold_funs false (||) fs
 
 (* negation of a test *)
 let not_fun f dc = not (f dc)
 
-(* using these combinators to define a selecion function for cards
- * whose data field is included in a given range *)
+(* using these combinators to define a selecion function
+ * for cards whose data field is included in a given range *)
 let date_interval db d1 d2 =
   and_fold [(after_dfield db d1 "Date") ; (before_dfield db d2 "Date")]
